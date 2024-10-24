49eef2376a83a22c3d0241d129bce5783d0a29bd
Allain Carl Mojado <ac.mojado@aclovestech.com>
2024-10-24 02:22:57 -0700

    Changelog: - [functionality] "Login with Spotify" button should now appear when the user hasn't logged in yet instead of the search bar - [functionality] Access Token: retrieving the data from Spotify and saving it to local storage - [functionality] Search: retriving the data from the Spotify API instead of using the mock search results data - [styling] Track: expand the track info to the right - [fix] Changed the + and - buttons in JSX from class to className to avoid errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,36 @@ import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import SearchResults from "./components/SearchResults";
 import Playlist from "./components/Playlist";
+import { saveTokenInfoFromHash, isTokenStillValid } from "./SpotifyModule";
 import "normalize.css";
 import "./global.css";
 import styles from "./styles/App.module.css";
 
 export default function App() {
-  // States
   const [searchResults, setSearchResults] = useState([]);
   const [playlist, setPlaylist] = useState({
     name: "My Playlist",
     tracks: [],
   });
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  // Effects
-
-  // Event Handlers
+  useEffect(() => {
+    if (!isTokenStillValid()) {
+      saveTokenInfoFromHash();
+    }
+    setIsAuthenticated(isTokenStillValid());
+  }, [isAuthenticated]);
 
   return (
     <div id="App">
       {/* Header */}
       <Header />
       {/* Search Bar and Search Button */}
-      <SearchBar setSearchResults={setSearchResults} />
+      <SearchBar
+        setSearchResults={setSearchResults}
+        isAuthenticated={isAuthenticated}
+        setIsAuthenticated={setIsAuthenticated}
+      />
       <div id="lists-container" className={styles.lists}>
         {/* Search Results */}
         <SearchResults
